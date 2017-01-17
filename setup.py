4588ed65ef7ea08dd75a5dd748b887b7f18671a0
Eric Busboom <eric@clarinova.com>
2017-01-17 12:55:49 -0800

    Hack install

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     long_description=readme,
     packages=packages,
     include_package_data=True,
-
+    zip_safe=False,
     install_requires=[
         'datapackage',
         'rowgenerators',
