97ac155c1cf3167c49561903ace0688b69836ec1
Eric Busboom <eric@clarinova.com>
2017-09-21 09:06:42 -0700

    Rev version and publish

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ classifiers = [
 
 setup(
     name='metapack',
-    version='0.6.10',
+    version='0.6.11',
     description='Data packaging system using Metatab',
     long_description=readme,
     packages=find_packages(),
