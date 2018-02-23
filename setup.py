c11c3ab27543b767f8e164109c9762a9095e9a24
Eric Busboom <eric@civicknowledge.com>
2018-02-22 16:37:00 -0800

    rev

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ classifiers = [
 
 setup_d = dict(
     name='metapack',
-    version='0.7.4',
+    version='0.7.5',
     description='Data packaging system using Metatab',
     long_description=readme,
     packages=find_packages(),
