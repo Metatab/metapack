931b5e1bbe828ace9c3892cc5c51a3b62d653b7b
Eric Busboom <eric@civicknowledge.com>
2018-04-09 09:33:09 -0700

    rev

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ classifiers = [
 # noinspection PyTypeChecker
 setup_d = dict(
     name='metapack',
-    version='0.7.9',
+    version='0.7.10',
     description='Data packaging system using Metatab',
     long_description=readme,
     packages=find_packages(),
