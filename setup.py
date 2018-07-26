35208bafadd3ec5f6d6db81d61557a7514b32f34
Eric Busboom <eric@civicknowledge.com>
2018-07-26 10:38:19 -0700

    Lot os little bugs

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ classifiers = [
 # noinspection PyTypeChecker
 setup_d = dict(
     name='metapack',
-    version='0.8.13',
+    version='0.8.14',
     description='Data packaging system using Metatab',
     long_description=readme,
     packages=find_packages(),
