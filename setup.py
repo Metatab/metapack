8c32d17b14c9c9684c9d167c0f8377cedf11687e
Eric Busboom <eric@civicknowledge.com>
2018-09-12 21:32:23 -0700

    Hacking travis

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ classifiers = [
 # noinspection PyTypeChecker
 setup_d = dict(
     name='metapack',
-    version='0.8.19',
+    version='0.8.20',
     description='Data packaging system using Metatab',
     long_description=readme,
     packages=find_packages(),
