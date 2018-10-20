14c8dfe8f9ec969da46d7f810f6a639808d6363d
Eric Busboom <eric@civicknowledge.com>
2018-10-20 06:29:00 -0700

    Fixing wayward imports

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ classifiers = [
 # noinspection PyTypeChecker
 setup_d = dict(
     name='metapack',
-    version='0.8.23',
+    version='0.8.24',
     description='Data packaging system using Metatab',
     long_description=readme,
     packages=find_packages(),
