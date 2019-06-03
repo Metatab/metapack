89d984c6ea9d5aac87943d49e26afdac8beddc3c
Eric Busboom <eric@civicknowledge.com>
2019-06-03 14:36:36 -0700

    random stuff

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ classifiers = [
 # noinspection PyTypeChecker
 setup_d = dict(
     name='metapack',
-    version='0.9.4',
+    version='0.9.5',
     description='Data packaging system using Metatab',
     long_description=readme,
     packages=find_packages(),
